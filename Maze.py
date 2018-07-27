b377d23a2c5b0201fd41855c832049c54bd54f73
Luiza Engler Stadelhofer <luiza.engler@gmail.com>
2018-07-27 12:07:27 -0300

    Maze solver working

--- Maze.py
+++ Maze.py
@@ -1,4 +1,4 @@
-import numpy, collections
+import numpy, collections, math
 from Queue import *
 from Graph import Graph
 
@@ -13,13 +13,12 @@ class Maze(object):
 		self.costSoFar = {}
 		self.cameFrom[self.graph.start] = None
 		self.costSoFar[self.graph.start] = 0
-		self.aStar()
 
 	def heuristic(self, a, b):
 		# Euclidean distance between point a and b
-		return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2))
+		return math.sqrt(pow(int(a[0]) - int(b[0]), 2) + pow(int(a[1]) - int(b[1]), 2))
 
-	def aStar(self):
+	def aStar(self, pixels):
 
 		while not self.listNodes.empty():
 			current = self.listNodes.get()
@@ -27,15 +26,15 @@ class Maze(object):
 			if current == self.graph.end:
 				print("Got to the end!!")
 				break
-
 			#print(self.graph.neighbors(current))
 
 			for next in self.graph.neighbors(current):
-				newCost = self.costSoFar[current] + self.graph.cost(current, next)
-				if next not in self.costSoFar or newCost < self.costSoFar[next]:
+				newCost = self.costSoFar[current] + self.graph.cost(current, next[1])
+				if next[1] not in self.costSoFar or newCost < self.costSoFar[next[1]]:
+					self.costSoFar[next[1]] = newCost
+					priority = newCost + self.heuristic(self.graph.end, next[1])
+					self.listNodes.put(next[1], priority)
+					self.cameFrom[next[1]] = current
 					print("Passed by " + current)
-					self.costSoFar[next] = newCost
-					priority = newCost + self.heuristic(self.graph.end, next)
-					self.listNodes.put(next, priority)
-					self.cameFrom[next] = current
+					pixels[int(current[1]), int(current[0])] = (255, 0, 0, 255)
 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ graph = Graph()
 image = Image.open('maze.png')
 pixels = image.load()
 print("Maze before:")
-#image.show()
+image.show()
 
 width, height = image.size
 
@@ -15,15 +15,16 @@ for x in xrange(0,height):
 	for y in xrange(0,width):
 		if pixels[y,x] == (255,255,255,255):
 			if x == 0:
-				graph.edges['0'+str(y)] = []
-				graph.start = '0'+str(y)
+				graph.edges[str(x)+str(y)] = []
+				graph.start = str(x)+str(y)
 				graph.numVertices = graph.numVertices + 1
+				added = True
 			elif x == height-1:
 				graph.edges[str(x)+str(y)] = []
 				graph.end = str(x)+str(y)
 				graph.numVertices = graph.numVertices + 1
+				added = True
 			else:
-				added = False
 				counter = 0
 				if pixels[y,x-1] == (255,255,255,255):
 					counter = counter + 1
@@ -55,34 +56,37 @@ for x in xrange(0,height):
 						graph.numVertices = graph.numVertices + 1
 						added = True
 
-				# Adds the edges to our graph
-				if added:
-					counter = 0
-					for z in reversed(range(0,y-1)):
-						if pixels[z,x] == (0,0,0,255):
+			# Adds the edges to our graph
+			if added:
+				counter = 0
+				for z in reversed(range(0,y)):
+					if pixels[z,x] == (0,0,0,255):
+						break
+					elif pixels[z,x] == (255,255,255,255):
+						counter = counter + 1
+						if str(x)+str(z) in graph.edges:
+							graph.edges[str(x)+str(y)].append([counter, str(x)+str(z)])
+							graph.edges[str(x)+str(z)].append([counter, str(x)+str(y)])
 							break
-						elif pixels[z,x] == (255,255,255,255):
-							counter = counter + 1
-							if str(x)+str(z) in graph.edges:
-								graph.edges[str(x)+str(y)].append([counter, str(x)+str(z)])
-								graph.edges[str(x)+str(z)].append([counter, str(x)+str(y)])
-								break
-					counter = 0
-					for z in reversed(range(0,x-1)):
-						if pixels[y,z] == (0,0,0,255):
+				if str(x)+str(y) == '53':
+						print "to aqui"
+				counter = 0
+				for z in reversed(range(0,x)):
+					if pixels[y,z] == (0,0,0,255):
+						break
+					elif pixels[y,z] == (255,255,255,255):
+						counter = counter + 1
+						if str(z)+str(y) in graph.edges:
+							graph.edges[str(x)+str(y)].append([counter, str(z)+str(y)])
+							graph.edges[str(z)+str(y)].append([counter, str(x)+str(y)])
 							break
-						elif pixels[y,z] == (255,255,255,255):
-							counter = counter + 1
-							if str(z)+str(y) in graph.edges:
-								graph.edges[str(x)+str(y)].append([counter, str(z)+str(y)])
-								graph.edges[str(z)+str(y)].append([counter, str(x)+str(y)])
-								break
-
+			added = False
+					
 print(graph.edges)
 maze = Maze(graph)
-maze.aStar()
+maze.aStar(pixels)
 
 #pix[x,y] = value
-#im.save('alive_parrot.png')
-#print("Maze after:")
-#image.show()
+image.save('maze.png')
+print("Maze after:")
+image.show()
